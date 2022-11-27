09f54da8af5d5217d561a56d83fc62132410e0f6
BreinerJT <breinerjtorres@gmail.com>
2022-11-27 16:52:35 -0500

    image optimizations

--- src/data.ts
+++ src/data.ts
@@ -4,28 +4,28 @@ export const projects: Project[] = [
 	{
 		body: 'Facilita la adopción de mascotas mediante la publicación de las mismas y la comunicación mediante chat de los usuarios.',
 		demo: 'https://petfinder-front.vercel.app/',
-		image: './petfinder.png',
+		image: './petfinder.webp',
 		link: 'https://github.com/BreinerJT/petfinder-front',
 		title: 'Petfinder'
 	},
 	{
 		body: 'Muestra el estado del clima en una ubicación dada. Además, cuenta con geolocalización para el estado del clima local.',
 		demo: 'https://weather-app-fang.vercel.app/',
-		image: './weather.png',
+		image: './weather.webp',
 		link: 'https://github.com/BreinerJT/WeatherApp',
 		title: 'Weather App'
 	},
 	{
 		body: 'Diario personal para guardar sucesos del día, también se pueden agregar imágenes relevantes de los sucesos.',
 		demo: 'https://journal-app-weld.vercel.app/',
-		image: './journal.png',
+		image: './journal.webp',
 		link: 'https://github.com/BreinerJT/journal-app',
 		title: 'Journal App'
 	},
 	{
 		body: 'Muestra más de 150 pokémons, también puedes guardar tus pokémons favoritos para verlos más luego.',
 		demo: 'https://nextpokemonstatic.vercel.app/',
-		image: './pokedex.png',
+		image: './pokedex.webp',
 		link: 'https://github.com/BreinerJT/next-pokemon-static',
 		title: 'Next Pokédex'
 	}
