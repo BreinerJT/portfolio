0957a695c9e339b19539f13090374230def76f8c
BreinerJT <breinerjtorres@gmail.com>
2022-12-23 09:28:41 -0500

    modify tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,11 +2,7 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {
-			boxShadow: {
-				'rounded': '1px 1px 10px 1px #fca5a5'
-			}
-		},
+		extend: {},
 		animation: {
 			'fadeIn': 'fadeIn 0.5s ease-in-out'
 		},
